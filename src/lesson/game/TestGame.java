095041ac80e392a8809cf5fd30b3c8b7a8f9ad48
Dmitrii Sobin <neo7976@yandex.ru>
2022-07-26 15:29:32 +0300

    Отработка лекции. Сериализация

---
+++ src/lesson/game/TestGame.java
@@ -0,0 +1,13 @@
+package lesson.game;
+
+public class TestGame {
+
+    public static void main(String[] args) {
+
+        GameProgress gameProgress =
+                new GameProgress(94, 10, 2, 254.32);
+
+
+
+    }
+}
