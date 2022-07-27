5309d9d590d60eff7386b6537e6a4e2c04a4cc5d
Dmitrii Sobin <neo7976@yandex.ru>
2022-07-27 11:13:13 +0300

    Написание zip. Удачная архивация нескольких файлов

--- src/homework/task2/Task2.java
+++ src/homework/task2/Task2.java
@@ -1,7 +1,6 @@
 package homework.task2;
 
 import java.io.*;
-import java.nio.file.Files;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -11,36 +10,31 @@ public class Task2 {
         GameProgress gameProgress2 = new GameProgress(65, 14, 8, 58.4);
         GameProgress gameProgress3 = new GameProgress(15, 2, 15, 102.9);
 
-        saveGame(gameProgress1, "save1");
-        saveGame(gameProgress2, "save2");
-        saveGame(gameProgress3, "save3");
+        saveGame(gameProgress1, "save1.data");
+        saveGame(gameProgress2, "save2.data");
+        saveGame(gameProgress3, "save3.data");
 
-        zipFiles("E:/JavaHome/savegames/", "save1", "save2", "save3");
+        zipFiles("E:/JavaHome/savegames/", "save1.data", "save2.data", "save3.data");
 
 
     }
 
-    private static void zipFiles(String zipWay, String... savesFile) {
+    private static void zipFiles(String zipWay, String... savesFile) throws IOException {
+        ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(zipWay + "zip.zip"));
         for (String saveFile : savesFile) {
-
-            try (ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(zipWay + "zip.zip"));
-                 FileInputStream fis = new FileInputStream(zipWay + saveFile + ".data")) {
-
-                ZipEntry entry = new ZipEntry(saveFile);
-                zos.putNextEntry(entry);
-                byte[] buffer = new byte[fis.available()];
-                fis.read(buffer);
-                zos.write(buffer);
-                zos.closeEntry();
-
-            } catch (IOException e) {
-                System.out.println(e.getMessage());
-            }
+            FileInputStream fis = new FileInputStream(zipWay + saveFile);
+            ZipEntry entry = new ZipEntry(saveFile);
+            zos.putNextEntry(entry);
+            byte[] buffer = new byte[fis.available()];
+            fis.read(buffer);
+            zos.write(buffer);
+            zos.closeEntry();
         }
+        zos.close();
     }
 
     private static void saveGame(GameProgress gameProgress1, String saveDate) {
-        try (FileOutputStream fos = new FileOutputStream("E:/JavaHome/savegames/" + saveDate + ".data");
+        try (FileOutputStream fos = new FileOutputStream("E:/JavaHome/savegames/" + saveDate);
              ObjectOutputStream oos = new ObjectOutputStream(fos)) {
             oos.writeObject(gameProgress1);
         } catch (IOException e) {
