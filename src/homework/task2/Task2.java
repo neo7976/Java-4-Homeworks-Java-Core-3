3e7b457a7526b939af9cffc26e23719c6eefcb31
Dmitrii Sobin <neo7976@yandex.ru>
2022-07-27 10:08:56 +0300

    Написание zip

--- src/homework/task2/Task2.java
+++ src/homework/task2/Task2.java
@@ -1,9 +1,8 @@
 package homework.task2;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class Task2 {
     public static void main(String[] args) {
@@ -15,10 +14,29 @@ public class Task2 {
         saveGame(gameProgress2, "save2");
         saveGame(gameProgress3, "save3");
 
+        zipFiles("E:/JavaHome/savegames/", "save1");
+
+
+    }
+
+    private static void zipFiles(String zipWay, String saveFile) {
+        try (ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(zipWay + "zip.zip"));
+             FileInputStream fis = new FileInputStream(zipWay+saveFile+".data")) {
+
+            ZipEntry entry = new ZipEntry(saveFile);
+            zos.putNextEntry(entry);
+            byte[] buffer = new byte[fis.available()];
+            fis.read(buffer);
+            zos.write(buffer);
+            zos.closeEntry();
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     private static void saveGame(GameProgress gameProgress1, String saveDate) {
-        try (FileOutputStream fos = new FileOutputStream("E:/JavaHome/savegames/"+ saveDate+".data");
+        try (FileOutputStream fos = new FileOutputStream("E:/JavaHome/savegames/" + saveDate + ".data");
              ObjectOutputStream oos = new ObjectOutputStream(fos)) {
             oos.writeObject(gameProgress1);
         } catch (IOException e) {
